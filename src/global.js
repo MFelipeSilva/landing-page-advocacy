e126c6d7b97dbb965ce79a95654bd43c3e29de0e
Felipe da Silva <silvaafelipe016@gmail.com>
2024-05-14 12:26:49 -0300

    feat: my first commit

---
+++ src/global.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+* {
+  border: 0;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  scroll-behavior: smooth;
+  font-family: Roboto, "Sans-serif";
+}
+`;
